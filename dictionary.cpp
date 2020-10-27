8b67f7c55584a242d9b7b07c6bc6aae4ae1a0ecc
Eric Dam <dam.eric3@gmail.com>
2020-10-27 16:44:15 -0700

    Initial commit

---
+++ dictionary.cpp
@@ -0,0 +1,4 @@
+//
+// Created by eric on 2020-10-27.
+//
+#include "dictionary.hpp"
---
+++ dictionary.hpp
@@ -0,0 +1,23 @@
+//
+// Created by eric on 2020-10-27.
+//
+#ifndef LAB6_DICTIONARY_HPP
+#define LAB6_DICTIONARY_HPP
+
+#include <map>
+
+using namespace std;
+class Dictionary {
+private:
+    map<string, string> words;
+
+public:
+    void storeWords();
+    void printDictionary();
+    void enterNewWord(string word, string def);
+    string findWordDef(string word);
+
+};
+
+
+#endif //LAB6_DICTIONARY_HPP
